616964a5bf7cf2183b2cb3019000cf2d3691f885
leonard-112 <leonardsidzinka112@gmail.com>
2025-08-11 13:36:33 +0200

    feat: insert images of math problems

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,14 +1,13 @@
-// Import the actual Figma assets for updated math questions
-import parallelLinesImageAsset from 'figma:asset/559dd80150f03e02c9c058d85896112e150e8439.png';
-import triangleImageAsset from 'figma:asset/9323426e87de25959be81e85d8425260508e7601.png';
-import rightTriangleImageAsset from 'figma:asset/5a4d2c37247795b3755e313a1eee8cfaa7010310.png';
-import histogramGroupsImageAsset from 'figma:asset/fd64413b188ae64ba99b7fa10acfe63c6beb9ef9.png';
-import histogramDataImageAsset from 'figma:asset/c43c61dd3dea722ef063b9bc4eb38c4e6188b6df.png';
-import co2AbsorptionGraphAsset from 'figma:asset/5f8db810e5a64e1ecb74c580a8e15e473665f9ec.png';
-import scatterplotMediumImageAsset from 'figma:asset/049bfe703e49482865f11a65549ff02b0fde8eba.png';
-import scatterplotHardImageAsset from 'figma:asset/3efcc0317349a0aad4d5b114fc045115ddec4da5.png';
-import probabilityTableImageAsset from 'figma:asset/d7cc66210ff2f0be3ebf588beaccd6381ca0b56d.png';
-import probabilityHardTableImageAsset from 'figma:asset/af2712d6fe861f2e5230c24fb1f18ee307550494.png';
+import parallelLinesImageAsset from '../assets/images/parallelLine.png';
+import triangleImageAsset from '../assets/images/triangle.png';
+import rightTriangleImageAsset from '../assets/images/rightTriangle.png';
+import histogramGroupsImageAsset from '../assets/images/histogramGroups.png';
+import histogramDataImageAsset from '../assets/images/histogramData.png';
+import co2AbsorptionGraphAsset from '../assets/images/co2Absorption.png';
+import scatterplotMediumImageAsset from '../assets/images/scatterplotMedium.png';
+import scatterplotHardImageAsset from '../assets/images/scatterplotHard.png';
+import probabilityTableImageAsset from '../assets/images/probabilityTable.png';
+import probabilityHardTableImageAsset from '../assets/images/probabilityHardTable.png';
 
 // Math question images
 export const parallelLinesImage = parallelLinesImageAsset;
@@ -22,16 +21,16 @@ export const scatterplotHardImage = scatterplotHardImageAsset;
 export const probabilityTableImage = probabilityTableImageAsset;
 export const probabilityHardTableImage = probabilityHardTableImageAsset;
 // Import the new math question images provided by user
-import linearFunctionTableImageAsset from 'figma:asset/eaf9bdf6b7d3dce195752bc01b3103d700c85fa7.png';
-import percentagesTableImageAsset from 'figma:asset/571df49647c3966e9dc90f6e5101135aa4a4d6ef.png';
+import linearFunctionTableImageAsset from '../assets/images/linearFunctionTable.png';
+import percentagesTableImageAsset from '../assets/images/percentagesTable.png';
 
 export const linearFunctionTableImage = linearFunctionTableImageAsset;
 export const percentagesTableImage = percentagesTableImageAsset;
 
 // Import Reading & Writing chart assets
-import gdpTableChartAsset from 'figma:asset/d8aa85b5db62b6b25c5351f101fed74fd7e40e63.png';
-import internetSpeedChartAsset from 'figma:asset/eb76e191d39e029b674510805085f236a40c74aa.png';
-import greenhouseGasChartAsset from 'figma:asset/07dd772d6215c78f825aa8ec63b84f52b0471bbe.png';
+import gdpTableChartAsset from '../assets/images/gdpTable.png';
+import internetSpeedChartAsset from '../assets/images/internetSpeedTable.png';
+import greenhouseGasChartAsset from '../assets/images/greenhouseGasChart.png';
 
 // Reading & Writing question chart images
 export const gdpTableChart = gdpTableChartAsset;
